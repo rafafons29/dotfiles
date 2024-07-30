15c1e1e7d2d914a25c291ddedb839add1623a179
rafafons29 <rafale02fc29@gmail.com>
2024-07-30 14:55:34 -0500

    Nada significante, solo se cambio el fondo

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -13,7 +13,7 @@ if [ $keybLayout = "be" ]; then
   cp $HOME/.config/qtile/config-azerty.py $HOME/.config/qtile/config.py
 fi
 
-feh --bg-fill ~/.wallpapers/115.jpg &
+feh --bg-fill ~/.wallpapers/214.png &
 
 run nm-applet &
 run pamac-tray &
