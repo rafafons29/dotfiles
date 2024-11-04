62d77d0b35a359eef2185bd25a1de402c517bfbd
rafafons29 <rafale02fc29@gmail.com>
2024-11-03 22:03:43 -0500

    Nueva animacion para cuando se abre una ventana

--- nvim/lua/config/plugins/neovide.lua
+++ nvim/lua/config/plugins/neovide.lua
@@ -1,2 +1,6 @@
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
+vim.g.neovide_cursor_smooth_blink = true
+
+vim.opt.guicursor =
+"n-v-c-sm:block-blinkwait700-blinkon600-blinkoff600,i-ci-ve:ver25-blinkwait600-blinkon600-blinkoff660,r-cr-o:hor20"
